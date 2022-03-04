91c891e283d85385643adcebc63f776744f24a02
DuongLuong2311 <duongthanhluong2311@gmail.com>
2022-03-04 14:50:49 +0700

    4/3/2022

--- js/main.js
+++ js/main.js
@@ -1,6 +1,74 @@
-$('.slider').slick({
+
+
+
+$(document).ready(function() {
+
+  $('.hamburger').click(function(e){
+    e.preventDefault();
+    $('.navbar-mobile').addClass('active');
+  })
+
+  $('.exit').click(function (e) { 
+    e.preventDefault();
+    $('.navbar-mobile').removeClass('active');
+  });
+
+
+  $('.slider').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
+    arrow: false,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1465,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 1140,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false,
+
+        }
+      },
+
+      {
+        breakpoint: 375,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false,
+
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   });
+  
+})
