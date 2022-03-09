0d3fb08713fc473d2b419a7cc47b84ee64e341fc
DuongLuong2311 <duongthanhluong2311@gmail.com>
2022-03-09 10:07:06 +0700

    fix perfect pixel 9/3/2022

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ $(document).ready(function() {
           slidesToShow: 2,
           slidesToScroll: 3,
           infinite: true,
-          dots: true
+          dots: false
         }
       },
       {
